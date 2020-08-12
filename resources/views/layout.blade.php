f6a3eb3112427e7f135c3a9c6c54041cd7165d38
Flavio <flavio.naves85@gmail.com>
2020-08-11 22:19:03 -0300

    Adicionado blade as views criadas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,19 +5,13 @@
     <meta http-equiv="Content-Language" content="pt-br">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
 </head>
-<title>Adicionar Série</title>
+<title>Controle de Séries</title>
 <body>
-    <div class="class container">
+<div class="class container">
         <div class="class jumbotron">
-            <h1>Adicionar Série</h1>
+            <h1>@yield('cabecalho')</h1>
         </div>
-        <form method="post">
-            <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" name="nome" id="nome"/>
-            </div>
-            <button class="btn btn-primary">Adicionar</button>
-        </form>
+        @yield('conteudo')
     </div>
 </body>
 </html>
