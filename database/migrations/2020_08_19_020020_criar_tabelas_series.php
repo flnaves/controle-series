fda1e0457ebab8ad5fd25cec2a97cfa37927a9af
Flavio <flavio.naves85@gmail.com>
2020-08-18 23:06:11 -0300

    Adicionado migration criar_tabelas_series

---
+++ database/migrations/2020_08_19_020020_criar_tabelas_series.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelasSeries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Criar tabela e campos
+        Schema::create('series', function (Blueprint $table) {
+            $table->string('nome');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Exclui a tabela
+        Schema::drop('series');
+    }
+}
